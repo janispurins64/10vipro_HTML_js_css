80dac0f519567cc4cfafc378ae9961f33fff97e8
janispurins64 <janispurin64@gmail.com>
2021-04-27 09:20:45 +0300

    Kvadrātvienādojums
    KVV sakņu noteik;sana, paredzot visus gadījumus

--- static/j10vi1.js
+++ static/j10vi1.js
@@ -1,4 +1,20 @@
-function jp_01(a,b){
+function jp_01(a,b,c){
+    let d;
+    d = b*b-4*a*c;
+    if(a === undefined || b === undefined || c === undefined){
+        console.log("Kāds no koeficientiem nav definēts");
+    }
+    else{
+        if(a===0 ){
+            console.log("Šis nav kvadrātvienādojums");        
+        }
+        else if(d<0){
+            console.log("Vienādojumam nav reālu sakņu");  
+        }
+        else{
+            console.log("x1=",(-b+Math.sqrt(d))/(2*a));
+            console.log("x2=",(-b-Math.sqrt(d))/(2*a));
+        }   
+    }
 
-    console.log(a,b,a+b);  
 }
